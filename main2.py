e2d9720f2da25141a09e74896b8eaa0fd6c4b313
skostousov <93064193+skostousov@users.noreply.github.com>
2023-03-20 08:19:27 -0500

    Added currency converter to main2

--- main2.py
+++ main2.py
@@ -2,6 +2,54 @@ import time
 import datetime
 import threading
 import student_class
+import requests #pip install requests
+from bs4 import BeautifulSoup #pip install bs4
+
+
+#The function needs 3 parameters, all for integer values. 
+#First value: 1 for USD, 2 for CAD, 3 for GBP, and 4 for EUR.
+#Second parameter is a 1 or 2-- A 1 lets us know that the final output should be in the foreign currency, a 2 for shekels
+#Last parameter specifies the actual value you wish to convert. If a 1 is specified in the second parameter, this parameter 
+#will be in shekels. If the second parameter is a 2, this will be in the specified foreign currency."""
+def convert(orig, to_or_from, value_orig):
+    cur=["EMPTY", 4500]
+    my_url='https://google.com'
+    spec_class='hello'
+    match orig:
+        case 1:
+            cur[0]="USD"
+            my_url='https://www.google.com/finance/quote/USD-ILS?sa=X&ved=2ahUKEwiDjLiU--b9AhXkmokEHdNqCEgQmY0JegQIBhAd'
+            spec_class="YMlKec fxKbKc"
+        case 2:
+            cur[0]="CAD"
+            my_url='https://www.google.com/finance/quote/CAD-ILS?sa=X&ved=2ahUKEwjq3cGLq-j9AhXGFFkFHbg7CDwQmY0JegQIBhAd'
+            spec_class="YMlKec fxKbKc"
+        #case 4:
+        #    cur[0]="EUR"
+        #    url='https://www.google.com/finance/quote/EUR-ILS?sa=X&ved=2ahUKEwjuyvisq-j9AhVBFFkFHfO2D68QmY0JegQIBhAd'
+        #    spec_class="YMlKec fxKbKc"
+        case 3:
+            cur[0]="GBP"
+            my_url='https://www.google.com/finance/quote/GBP-ILS?sa=X&ved=2ahUKEwjBiaHNq-j9AhUzVTUKHYb6Cj8QmY0JegQIBhAd'
+            spec_class="YMlKec fxKbKc"
+        case _:
+            print("ERROR: Please try again")
+    res= requests.get(my_url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    get_value = soup.find_all(class_=spec_class)[0].text
+    conversion_factor=float(get_value)
+    match to_or_from:
+        case 2:
+            cur[0]="SHKL"
+            cur[1]=(conversion_factor*value_orig)
+        case 1:
+            cur[1]=(value_orig/conversion_factor)
+        case _:
+            print("ERROR")
+    return(cur)
+
+
+
 
 # Class to create instance of inventroy item. Respectively, the parameters are as follows:
 # 1. NAME: The name of the item, 2. HUMAN: the name of the organizer, 3. MODE: the mode (1 for bulk orders, 2 for used items), 
@@ -162,10 +210,11 @@ class Operation:
             
 while True:
     print("Welcome to the Yeshiva Marketplace")
-    Mode = int(input("Please select the following options: \n 1. Group Order \n 2. Buy/Sell\n"))
-    category=  input("Please enter one of the following categories: Food, Amenities, Books/Sefarim\n")
-    program = Operation(Mode, category)
-    program.printavailableinventory()
+    Mode = int(input("Please select the following options: \n 1. Group Order \n 2. Buy/Sell\n 3.Currency Converter\n"))
+    if Mode==1 or Mode==2:    
+        category=  input("Please enter one of the following categories: Food, Amenities, Books/Sefarim\n")
+        program = Operation(Mode, category)
+        program.printavailableinventory()
     if Mode==1:    
         choice = int(input("1. Join Order\n2. Create Order\n"))
         if choice == 1:
@@ -184,6 +233,12 @@ while True:
             print(Operation.Students)
         elif choice == 2:
             program.buy()
+    elif Mode ==3:
+        currency=float(input("Please select one: 1.USD 2.CAD 3.GBP "))
+        convert_dir=float(input("Select original currency: 1.SHKL 2.Other"))
+        my_value=float(input("Value:"))
+        print(str(round(convert(currency, convert_dir, my_value)[1], 2)))
+        print(convert(currency, convert_dir, my_value)[0])
     for key, value in Operation.Inventory.items():
         if value.duration == 0:
             del Operation.Inventory[key]
