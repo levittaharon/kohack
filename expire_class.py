ca2d6ac432029f9807a6d980e886cf18a666e821
levittaharon <91753268+levittaharon@users.noreply.github.com>
2023-03-19 21:29:38 -0400

    Update expire_class.py

--- expire_class.py
+++ expire_class.py
@@ -1,7 +1,9 @@
+from catalogue_class import catalogue
 import sqlite3
 class expire:
     def __init__(self,category):
         self.category = category
+
     
     def check_expired(self):
         con = sqlite3.connect("students.db")
@@ -12,18 +14,23 @@ class expire:
         con.close()
     
     def check_stock(self):
+        #check if item is still in stock and if not it needs to be deleted and the owner must be notified
         con = sqlite3.connect("students.db")
         cur = con.cursor()
-        cur.execute(f"SELECT item_name,seller_name FROM {self.category} WHERE amount_left IS '0';")
-        info = cur.fetchall()  
-        print(info)      
-        cur.execute(f"DELETE FROM {self.category} WHERE amount_left IS '0';")
+        #if stock is 0 then it needs to go
+        cur.execute(f"SELECT item_name,seller_name FROM {self.category} WHERE amount_left IS ? ;",(0))
+        info = cur.fetchall()        
+        cur.execute(f"DELETE FROM {self.category} WHERE amount_left IS ? ;",(0))
         con.commit()
         con.close()
         
-        return(info)
+        for i in info: #make a list for each data set to pass in 
+            message = list(i)
+            catalogue().send_notification(message)
+            
 """
 #thes are all for testing
 instance = expire("books")
 instance.check_stock()
 """
+
