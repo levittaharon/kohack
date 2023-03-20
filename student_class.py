20f47654ec009913bdf867f08f269c3a8e188776
levittaharon <91753268+levittaharon@users.noreply.github.com>
2023-03-20 13:50:09 -0400

    Update student_class.py
    change db to directory.db and table to sdirectory

--- student_class.py
+++ student_class.py
@@ -10,15 +10,15 @@ class student:
   #this function takes in additional info about the student and adds it to the db
   def new_student(self,email,phone):
     #this is the db that will contain everything it's just simpler that way
-    conn = sqlite3.connect("students.db")
+    conn = sqlite3.connect("directory.db")
     cur = conn.cursor()
     #create a table for basic information of a student
-    cur.execute("CREATE TABLE IF NOT EXISTS directory(name TEXT NOT NULL PRIMARY KEY,password,email,phone,notification TEXT NOT NULL DEFAULT '',orders_part_of TEXT NOT NULL DEFAULT '');")
+    cur.execute("CREATE TABLE IF NOT EXISTS sdirectory(name TEXT NOT NULL PRIMARY KEY,password,email,phone,notification TEXT NOT NULL DEFAULT '',orders_part_of TEXT NOT NULL DEFAULT '');")
     #check for invalid password to display correct error message
     if self.password != "":
     #this is in a try except statement because if the data isn't correct then we need to send the user an error message
       try:
-        cur.execute("INSERT INTO directory(name,password,email,phone) VALUES(?,?,?,?);",(self.name,self.password,email,phone))
+        cur.execute("INSERT INTO sdirectory(name,password,email,phone) VALUES(?,?,?,?);",(self.name,self.password,email,phone))
         conn.commit()
         return True
       except:
@@ -30,18 +30,17 @@ class student:
       print("invalid password")
       return False
     #close the cursor because it is good practice if it is unnecessary to have it open
-    conn.close()
+    cur.close()
   
   #this will check if it is a valid login and return all of the information for now
   def login(self):
     #this is necessary to access the cursor
-    conn = sqlite3.connect("students.db")
+    conn = sqlite3.connect("directory.db")
     cur = conn.cursor()
     
-    cur.execute("SELECT * FROM directory WHERE name IS ? AND password IS ?;",(self.name,self.password))
+    cur.execute("SELECT * FROM sdirectory WHERE name IS ? AND password IS ?;",(self.name,self.password))
     #check if the previous statement found a login matching the one submitted
     result = cur.fetchall()
-    conn.close()
     if len(result)== 0: #if it doesn't find anything then len is 0
       print("bad login")
       return False
@@ -49,3 +48,10 @@ class student:
       
       print(result)
       return True
+
+  
+
+    
+#test
+#john = student("john","goodpass")
+#john.new_student("email",773)
